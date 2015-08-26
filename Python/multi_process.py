b498a77d02355b2378e367a5c30fdeeac19f368d
Ernest Beličič <ernest.belicic@gmail.com>
2015-08-26 10:22:36 +0200

    set camera attributes

--- Python/multi_process.py
+++ Python/multi_process.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+import httplib
 import urllib
 from cStringIO import StringIO
 from multiprocessing import Process, Queue, Lock
@@ -16,7 +17,27 @@ class ImagePull(Process):
 		self.queue = queue
 		self.ioLock = ioLock
 		self.ip = params["ip"]
+		self.name = params["name"]
+		self.httpConn = httplib.HTTPConnection(self.ip)
 		self.daemon = True
+		
+		#set camera attributes
+		print self.name + ": setting attributes ..."
+		self.CGIquery("brightness=" + str(params["brightness"]))
+		self.CGIquery("speed=" + str(params["speed"]))
+		self.CGIquery("pan=" + str(params["pan"]))
+		self.CGIquery("tilt=" + str(params["tilt"]))
+		finishedPan = False
+		finishedTilt = False
+		while not finishedPan or not finishedTilt:
+			pos = self.CGIquery("query=position").split("\n")
+			finishedPan = float(pos[0].split("=")[1]) == float(params["pan"])
+			finishedTilt = float(pos[1].split("=")[1]) == float(params["tilt"])
+		print self.name + ": done\n"
+	
+	def CGIquery(self, query):
+		self.httpConn.request("GET", "/axis-cgi/com/ptz.cgi?" + query)
+		return self.httpConn.getresponse().read()
 	
 	def run(self):
 		stream = urllib.urlopen("http://" + self.ip + "/mjpg/video.mjpg")
@@ -47,10 +68,10 @@ if __name__ == "__main__":
 	camParams = []
 	
 	#ip, pan, tilt
-	camAttributes = [("192.168.1.131", 123, 123),
-					 ("192.168.1.128", 123, 123),
-					 ("192.168.1.121", 123, 123),
-					 ("192.168.1.114", 123, 123)];
+	camAttributes = [("192.168.1.131", 120, 30),
+					 ("192.168.1.128", 111, 30),
+					 ("192.168.1.121", 159, 30),
+					 ("192.168.1.114", 42, 24)];
 		   
 	#intrinsics, extrinsics, radial
 	matParams = sio.loadmat("cameraParams_py")
@@ -59,9 +80,11 @@ if __name__ == "__main__":
 						  "extrinsics": matParams["params"][0][i][1],
 						  "radial": matParams["params"][0][i][2],
 						  "brightness": 2345,
+						  "speed": 100,
 						  "ip": camAttributes[i][0],
 						  "pan": camAttributes[i][1],
-						  "tilt": camAttributes[i][2]
+						  "tilt": camAttributes[i][2],
+						  "name": "camera" + str(i + 1)
 						 });
 		
 								
@@ -73,6 +96,8 @@ if __name__ == "__main__":
 		p = ImagePull(q, ioLock, param)
 		queues.append(q)
 		processes.append(p)
+	
+	for p in processes:
 		p.start()
 	
-	time.sleep(10)
+	time.sleep(20)
